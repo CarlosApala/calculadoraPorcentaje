24a1a99101977395b5a12368487184c0c54a410c
solra <solra@DESKTOP-OQ3EKU5>
2022-04-30 11:43:05 -0400

    'finalizado'

--- app/src/main/java/com/example/myapplication4/MainActivity.kt
+++ app/src/main/java/com/example/myapplication4/MainActivity.kt
@@ -3,22 +3,41 @@ package com.example.myapplication4
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.TextView
+import com.example.myapplication4.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
-    private var valor1=0;
+
+    private lateinit var binding:ActivityMainBinding
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener{calculateTip()}
+    }
 
-
-
-
+    private fun calculateTip(){
+        val stringIntField=binding.costOfService.text.toString()
+        val cost=stringIntField.toDoubleOrNull()
+        if(cost==null){
+            binding.tipResult.text="";
+            return
+        }
+        val tipPorcentaje=when(binding.tipOptions.checkedRadioButtonId){
+            R.id.option_twenty_percent->0.20
+            R.id.option_eighteen_percent->0.18
+            else->0.15
+        }
+        var tip=tipPorcentaje*cost
+        val roundUp=binding.roundUpSwitch.isChecked
+        if(roundUp){
+            tip=kotlin.math.ceil(tip)
+        }
+        val formattedTip=NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text=getString(R.string.tip_amount,formattedTip)
     }
 
 }
