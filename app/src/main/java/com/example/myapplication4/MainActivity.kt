d97c2fcb7f6666f0e5a744f6112be425b2452731
solra <solra@DESKTOP-OQ3EKU5>
2022-04-30 09:59:33 -0400

    inicializacion de calculadora

---
+++ app/src/main/java/com/example/myapplication4/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication4
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private var valor1=0;
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+    }
+
+}
